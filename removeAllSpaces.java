a5ced4b97487f80c8863515947f58b23fa2824e7
HugoSanche <hbmclaren@gmail.com>
2025-01-25 14:10:15 -0600

    add example how remove white spaces in a string

--- removeAllSpaces.java
+++ removeAllSpaces.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class removeAllSpaces {
+public class removeAllSpaces {
+    public static void main(String[] args) {
+        String sentence = "  Guillermo Hugo  Baltazar Sanchez   ";
+
+        String word = removeAllWhiteSpaces(sentence);
+
+        System.out.println(word);
+    }
+
+    public static String removeAllWhiteSpaces(String sentence) {
+        return sentence.replaceAll("\\s","");
+
+    }
+
+    public static String removeAllWhiteSpaces2(String sentence) {
+
+        if(sentence==null || sentence.isEmpty()){
+            return "";
+        }
+
+        sentence=sentence.trim();
+        String[] strArray = sentence.trim().split(" ");
+        StringBuilder strB=new StringBuilder();
+
+       for(String word:strArray){
+           strB.append(word.trim());
+       }
+       return strB.toString();
+    }
 }
