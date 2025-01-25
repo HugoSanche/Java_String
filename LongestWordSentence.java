ae59120a1d532f4929929c506aa091555144f3b3
HugoSanche <hbmclaren@gmail.com>
2025-01-25 14:11:57 -0600

    add example how remove white spaces in a strin

--- LongestWordSentence.java
+++ LongestWordSentence.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class LongestWordSentence {
+public class LongestWordSentence {
+    public static void main(String[] args) {
+        String  sentence="Guiller Hugo Baltazar Sanchez";
+
+        String word=getLongestWord2(sentence);
+
+        System.out.println(word);
+    }
+    public static String getLongestWord(String sentence){
+        if(sentence==null || sentence.isEmpty()){
+            return "";
+        }
+
+        String[] strArray=sentence.trim().split(" ");
+
+        int value=0;
+        for(int i=1;i<strArray.length;i++){
+            if(strArray[value].length()<strArray[i].length()){
+                value=i;
+            }
+        }
+        return strArray[value];
+        }
+    public static String getLongestWord2(String sentence){
+
+        if(sentence==null || sentence.isEmpty()){
+            return "";
+        }
+
+        String[] strArray=sentence.trim().split(" ");
+
+        String longestWord="";
+       for(String word:strArray){
+           if(word.length()>longestWord.length()){
+               longestWord=word;
+           }
+       }
+       return longestWord;
+    }
 }
--- Utilidades.java
+++ Utilidades.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Utilidades {
+public class Utilidades {
+    public static void main(String[] args) {
+        String str="Hugo Baltazar";
+        String str2="Sanchez";
+
+        int tamaño=str.length();
+
+        //substring
+        String extract=str.substring(5,13);
+        System.out.println(extract);
+
+        //concat
+        String str3=str.concat(" ").concat(str2);
+        System.out.println(str3);
+
+        //indexOf
+        //si no encuentra el caracter devuelve -1
+        System.out.println(str.indexOf('a'));
+
+        //Equals
+        String apellido="Sanchez";
+        System.out.println(str.equals(str2));
+        System.out.println(str2.equals(apellido));
+
+        //charAt
+        char c=str.charAt(0);
+        System.out.println(c);
+
+        //convertirlo a array
+        char[]array=str.toCharArray();
+        System.out.println(array);
+        for(char value:array){
+            System.out.print(value);
+        }
+        System.out.println();
+        System.out.println("-----------------------------------------");
+
+        //trim
+        //quita espacios al inicio y al final. No quita espacios en medio
+        //System.out.println(str.trim());
+        String space="  Hugo  ";
+        System.out.println(space.trim());
+
+        System.out.println("--------------------REEMPLACE--------------------");
+        String nombre="Carolina";
+        String nombreCompleto="Hugo Baltazar";
+
+
+        String strReEmplace=nombre.replace(nombre,"Lourdes");
+        System.out.println(strReEmplace);
+
+        String strReempleceNombreCompleto=nombreCompleto.replace("Baltazar","Sanchez");
+        System.out.println(strReempleceNombreCompleto);
+        //output: Hugo Sanchez
+
+        System.out.println("---------------------------SPLIT----------------------------");
+        //SPLIT
+       String [] parts=str.trim().split(" ");
+
+       for(String part:parts){
+           System.out.println(part);
+       }
+    }
 }
