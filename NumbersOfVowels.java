e58fe5a74acf25321fe31500b6e9de9c87a359f1
HugoSanche <hbmclaren@gmail.com>
2025-01-25 14:09:34 -0600

    add example of get numbers of vowels

--- NumbersOfVowels.java
+++ NumbersOfVowels.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class NumbersOfVowels {
+public class NumbersOfVowels {
+    public static void main(String[] args) {
+        String sentence = "Guillermo Hugo Baltazar Sanchez";
+
+        int numbersOfVowel = getNumbersOfVowel(sentence);
+
+        System.out.println(numbersOfVowel);
+    }
+
+    public static int getNumbersOfVowel(String sentence){
+       String vowels="aeiouAEIOU";
+       int count=0;
+       for(int i=0; i<sentence.length();i++){
+           char ch=sentence.charAt(i);
+
+           //check if caracter is a vowel
+           if(vowels.indexOf(ch)!=-1){
+               count++;
+           }
+       }
+        return count;
+    }
 }
